d1fffefe96c487ff31c7c0811211cef8a62f17a8
Curtis Rueden <ctrueden@wisc.edu>
2016-08-03 17:22:22 -0500

    Types: allow creating multidimensional array types

--- src/test/java/org/scijava/util/TypesTest.java
+++ src/test/java/org/scijava/util/TypesTest.java
@@ -136,6 +136,7 @@ public class TypesTest {
 	/** Tests {@link Types#array}. */
 	@Test
 	public void testArray() {
+		// 1-dimensional cases
 		assertSame(boolean[].class, Types.array(boolean.class));
 		assertSame(String[].class, Types.array(String.class));
 		assertSame(Number[].class, Types.array(Number.class));
@@ -145,6 +146,16 @@ public class TypesTest {
 		try {
 			Types.array(void.class);
 			fail("Unexpected success creating void[]");
+		}
+		catch (final IllegalArgumentException exc) { }
+
+		// multidimensional cases
+		assertSame(Number[][].class, Types.array(Number.class, 2));
+		assertSame(boolean[][][].class, Types.array(boolean.class, 3));
+		assertSame(String.class, Types.array(String.class, 0));
+		try {
+			Types.array(char.class, -1);
+			fail("Unexpected success creating negative dimensional array");
 		}
 		catch (final IllegalArgumentException exc) { }
 	}
