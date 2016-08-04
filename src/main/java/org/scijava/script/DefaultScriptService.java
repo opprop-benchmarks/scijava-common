b12842b391857ff5037e22f687b3b016aaab1624
Curtis Rueden <ctrueden@wisc.edu>
2016-08-04 08:44:21 -0500

    Eliminate now-redundant private method
    We can now use Types.array(Class, int) instead.

--- src/main/java/org/scijava/script/DefaultScriptService.java
+++ src/main/java/org/scijava/script/DefaultScriptService.java
@@ -67,6 +67,7 @@ import org.scijava.service.Service;
 import org.scijava.util.ClassUtils;
 import org.scijava.util.ColorRGB;
 import org.scijava.util.ColorRGBA;
+import org.scijava.util.Types;
 
 /**
  * Default service for working with scripts.
@@ -203,7 +204,7 @@ public class DefaultScriptService extends
 		final Class<?> type = aliasMap().get(componentAlias);
 		if (type != null) {
 			final int arrayDim = (alias.length() - componentAlias.length()) / 2;
-			return makeArrayType(type, arrayDim);
+			return Types.array(type, arrayDim);
 		}
 
 		try {
@@ -404,10 +405,4 @@ public class DefaultScriptService extends
 		if (!alias.endsWith("[]")) return alias;
 		return stripArrayNotation(alias.substring(0, alias.length() - 2));
 	}
-
-	private Class<?> makeArrayType(final Class<?> type, final int arrayDim) {
-		if (arrayDim <= 0) return type;
-		return makeArrayType(ClassUtils.getArrayClass(type), arrayDim - 1);
-	}
-
 }
